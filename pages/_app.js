ff956b5d57861bfcb1fdaa3d1fc5ee0ace1bd4c7
Miranda Liu <mirandaliu2001@gmail.com>
2020-11-06 17:05:13 -0500

    changed favicon

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import '../styles/globals.css'
+import Head from "next/head"
 
 export default function App (props) {
   
@@ -32,6 +33,19 @@ export default function App (props) {
 
   return (
     <React.Fragment>
+
+      {/* displays website favicon */}
+
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
+        <meta name="msapplication-TileColor" content="#da532c"/>
+        <meta name="theme-color" content="#ffffff"/>
+      </Head>
+
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
